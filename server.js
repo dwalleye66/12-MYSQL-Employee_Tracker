8703ec828e428d762766f34fd7014ec9e8ee4b29
Darin Wallace <dwalleye66@gmail.com>
2021-03-02 23:54:52 -0600

    final server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const connection = mysql.createConnection({
   user: "root",
 
   password: "Zabokrtsky+1",
-  database: "employees_trackerdb",
+  database: "employee_trackerdb",
 });
 
 connection.connect(function (err) {
